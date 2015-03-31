5a5a5798dd103102b8fdbcd2f4144b40a6121dfd
Garret Buell <gmbuell@gmail.com>
2015-03-31 15:57:46 -0700

    Render a COFile.

--- Clobelisk/Components/COFileComponent.mm
+++ Clobelisk/Components/COFileComponent.mm
@@ -11,4 +11,37 @@
 
 static CKComponent *cofileComponent(COFile *file)
 {
+  return [CKStackLayoutComponent
+           newWithView:{}
+ size:{}
+ style:{}
+ children:{
+      {
+      [CKLabelComponent
+        newWithLabelAttributes:{
+          .string = file.text,
+              .font = [UIFont fontWithName:@"Baskerville" size:30]
+        }
+     viewAttributes:{
+          {@selector(setBackgroundColor:), [UIColor clearColor]},
+          {@selector(setUserInteractionEnabled:), @NO},
+        }
+       ],
+          .alignSelf = CKStackLayoutAlignSelfCenter
+          },
+      {
+      [CKLabelComponent
+        newWithLabelAttributes:{
+          .string = file.author,
+              .font = [UIFont fontWithName:@"Verdana" size:20]
+        }
+     viewAttributes:{
+          {@selector(setBackgroundColor:), [UIColor clearColor]},
+          {@selector(setUserInteractionEnabled:), @NO},
+        }
+       ],
+          .alignSelf = CKStackLayoutAlignSelfEnd
+          }
+    }
+          ]
 }
